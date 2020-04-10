3a7a275febd8a4a4aea363a812d5b905e879d7a1
parvez218 <parvezakhtar218@gmail.com>
2020-04-10 14:14:30 +0530

    project-structure

---
+++ src/main/java/com/sapient/CheckEven.java
@@ -0,0 +1,16 @@
+package com.sapient;
+
+/**
+ * Check even
+ *
+ */
+public class CheckEven {
+	public Boolean check(int n) {
+		if (n % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
